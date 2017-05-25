ec48af45d25efbca8d628bf2494220e59d5ff4e7
chengcheng <chengcheng@haixianglicai.com>
2017-05-25 12:24:43 +0800

    项目启动 success

--- platform-erp/src/main/java/com/cc/mapper/DemoMapper.java
+++ platform-erp/src/main/java/com/cc/mapper/DemoMapper.java
@@ -1,7 +1,6 @@
 package com.cc.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Repository;
 
 /**
  * Created by Chengcheng on 2017/5/24.
---
+++ platform-repository/src/main/java/com/cc/config/MyBatisConfig.java
@@ -0,0 +1,36 @@
+package com.cc.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by Chengcheng on 2017/5/25.
+ */
+@Configuration
+@ComponentScan("com.cc.mapper")
+public class MyBatisConfig {
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactoryBean() throws Exception{
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource());
+
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath:mapping/*.xml"));
+        return sqlSessionFactoryBean.getObject();
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource(){
+        return new DruidDataSource();
+    }
+}
